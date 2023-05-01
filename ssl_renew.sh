36b374bc5fe8b8e8dbbb2b05b4ce820eba0d3af8
Vivek Shukla <viv3kshukla@gmail.com>
2023-05-02 02:49:28 +0530

    add comments

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -7,6 +7,9 @@ cd /home/ubuntu/wordpress-on-docker/
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
 
-# chmod +x ssl_renew.sh
+# # copy this file in your home directory because after chmod +x, it will be shown 
+# # as changed in git
+# cp ssl_renew.sh ~/ssl_renew.sh
+# chmod +x ~/ssl_renew.sh
 # sudo crontab -e
-# 0 12 * * * /home/ubuntu/wordpress-on-docker/ssl_renew.sh >> /var/log/cron.log 2>&1
+# 0 12 * * * /home/ubuntu/ssl_renew.sh >> /var/log/cron.log 2>&1
