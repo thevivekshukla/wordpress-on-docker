454ee2febcf300c81515a3f217a0abcab55f4fa5
Vivek Shukla <viv3kshukla@gmail.com>
2023-05-02 02:28:00 +0530

    add ssl renew script

---
+++ ssl_renew.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COMPOSE="/usr/bin/docker-compose --ansi never"
+DOCKER="/usr/bin/docker"
+
+cd /home/ubuntu/wordpress-on-docker/
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
+$DOCKER system prune -af
+
+# sudo crontab -e
+# 0 12 * * * /home/ubuntu/wordpress-on-docker/ssl_renew.sh >> /var/log/cron.log 2>&1
